155bf0d6e855bcdcdaf358d599660f958ecf2221
edol459 <dolder459@gmail.com>
2025-07-30 22:18:26 -0400

    gunicorn

--- server.py
+++ server.py
@@ -66,4 +66,5 @@ def outliers(game_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
